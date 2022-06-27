9bb86ce6bb4e3e192aea70a9c1dbf6c5d4db7205
AlexLopezDevelop <alex_lopez.es@icloud.com>
2022-06-27 16:48:44 +0200

    added: implementing friend api calls

--- routes/controllers/friend.js
+++ routes/controllers/friend.js
@@ -1,16 +1,60 @@
+const mysql = require("mysql2");
+
 const getAllFriendsRequests = async (req, res) => {
   try {
+    const { USER_ID } = req
+
+    const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+    connection.query(
+      "SELECT t2.id, t2.name, t2.last_name, t2.email, t2.image FROM `friends` t1 INNER JOIN users t2 ON t1.user_id = t2.id WHERE t1.user_id_friend = ? ORDER BY id DESC",
+      [USER_ID],
+      function (err, results, fields) {
+
+        if (err) res.status(400);
+        if (results.length === 0) {
+          res.status(400)
+        }
+
+        res.status(200).json(results)
+      }
+    );
+
+    connection.end();
 
   } catch (e) {
-
+    res.status(400).json({
+      message: e
+    })
   }
 }
 
 const getAllFriends = async (req, res) => {
   try {
+    const { USER_ID } = req
+
+    const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+    connection.query(
+      "SELECT t2.id, t2.name, t2.last_name, t2.email, t2.image FROM `friends` t1 INNER JOIN users t2 ON t1.user_id_friend = t2.id WHERE user_id = ? ORDER BY id DESC",
+      [USER_ID],
+      function (err, results, fields) {
+
+        if (err) res.status(400);
+        if (results.length === 0) {
+          res.status(400)
+        }
+
+        res.status(200).json(results)
+      }
+    );
+
+    connection.end();
 
   } catch (e) {
-
+    res.status(400).json({
+      message: e
+    })
   }
 }
 
--- routes/friend.js
+++ routes/friend.js
@@ -1,10 +1,13 @@
 const { getAllFriendsRequests, getAllFriends, createFriendRequest, acceptFriendRequest, rejectFriendRequest } = require("./controllers/friend");
+const { privateRoute } = require("../middleware/privateRoute");
 const friendSettings = require('express').Router({ mergeParams: true });
+const bodyParser = require('body-parser').json();
 
-friendSettings.get("/requests", getAllFriendsRequests)
-friendSettings.get("", getAllFriends)
-friendSettings.post("/{id}", createFriendRequest)
-friendSettings.put("/{id}", acceptFriendRequest)
-friendSettings.delete("/{id}", rejectFriendRequest)
+
+friendSettings.get("/requests", privateRoute, bodyParser, getAllFriendsRequests)
+friendSettings.get("", privateRoute, bodyParser, getAllFriends)
+friendSettings.post("/:id", createFriendRequest)
+friendSettings.put("/:id", acceptFriendRequest)
+friendSettings.delete("/:id", rejectFriendRequest)
 
 module.exports = friendSettings;
